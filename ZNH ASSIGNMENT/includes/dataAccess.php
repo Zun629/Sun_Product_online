f79d82bbd10d5554edef38e7842af97025849790
Zun629 <zunnandarhtun@gmail.com>
2024-11-29 09:03:23 +0100

    Update dataAccess.php

--- ZNH ASSIGNMENT/includes/dataAccess.php
+++ ZNH ASSIGNMENT/includes/dataAccess.php
@@ -36,7 +36,7 @@ class DataAccess extends Database{
                     return "Wrong Password";
                 }
             }else{
-                return "NO USER WITH THIS NAME/EMAIL FOUND!";
+                return "NO USER WITH THIS EMAIL FOUND!";
             }
             
         }
@@ -295,13 +295,10 @@ class DataAccess extends Database{
             // 16 bits for "time_mid"
             mt_rand( 0, 0xffff ),
     
-            // 16 bits for "time_hi_and_version",
-            // four most significant bits holds version number 4
+            
             mt_rand( 0, 0x0fff ) | 0x4000,
     
-            // 16 bits, 8 bits for "clk_seq_hi_res",
-            // 8 bits for "clk_seq_low",
-            // two most significant bits holds zero and one for variant DCE1.1
+            
             mt_rand( 0, 0x3fff ) | 0x8000,
     
             // 48 bits for "node"
